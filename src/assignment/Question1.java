881551edf96d320bf7d8a5f034418074e22ec346
sehrigokcan <sehrigokcan@gmail.com>
2022-12-08 18:59:02 +0100

    'Homework1'

---
+++ src/assignment/Question1.java
@@ -0,0 +1,94 @@
+package assignment;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 2- Create 3 variables for every primitive data type, and print 
+	them on the console.
+		 */
+		
+		//1 
+		
+		byte age=31;
+		System.out.println(age);
+		
+		short populationOfMyVillage=1234;
+		System.out.println(populationOfMyVillage);
+
+		int myNummer=123456;
+		System.out.println(myNummer);
+
+		long score1= 56434432435L;
+		System.out.println(score1);
+
+		float score2= 98.41242423F;
+		System.out.println(score2);
+
+		double height=71.5;
+		System.out.println(height);
+
+		boolean isMarried=true;
+		System.out.println(isMarried);
+
+		char group='Z';
+		System.out.println(group);
+		
+		
+		
+		//2
+		
+		byte nummer=65;
+		System.out.println(nummer);
+		
+		short count=12345;
+		System.out.println(count);
+
+		int a=123456;
+		System.out.println(a);
+
+		long score= 2353534646L;
+		System.out.println(score);
+
+		float pi= 3.14F;
+		System.out.println(pi);
+
+		double weight=71.5;
+		System.out.println(weight);
+
+		boolean isOnline=true;
+		System.out.println(isOnline);
+
+		char myLetter='f';
+		System.out.println(myLetter);
+		
+		
+		//3 
+		
+		byte x=127;
+		System.out.println(x);
+		
+		short b=12345;
+		System.out.println(b);
+
+		int c=543345342;
+		System.out.println(c);
+
+		long d= 3463465747457454626L;
+		System.out.println(d);
+
+		float e= 10002.41242423F;
+		System.out.println(e);
+
+		double f=65.856856845755345345;
+		System.out.println(f);
+
+		boolean g=false;
+		System.out.println(g);
+
+		char h='%';
+		System.out.println(h);
+
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sehri
+ *
+ */
+module assignment {
+}
